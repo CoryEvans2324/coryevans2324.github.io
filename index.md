40d130465e247d7c4e5489c621a30a1f3d3e804c
Cory Evans <cory0x45@gmail.com>
2020-03-11 10:10:42 +1300

    changed to jekyll

---
+++ index.md
@@ -0,0 +1,8 @@
+# Hello!
+
+Below is a simple python program.
+
+```python
+if __name__ == '__main__':
+    print('Hello world')
+```
--- js/index.js
+++
@@ -1,8 +0,0 @@
-function set_time() {
-    document.getElementById('time').innerText = Date();
-}
-
-set_time();
-setInterval(() => {
-    set_time();
-}, 1000);
